fa30ab80646afdb9cdca0030e7ae0487b2762130
imck037 <chandradhar.2003@gmail.com>
2026-03-11 11:26:00 +0530

    ftp client is working with raw commands...

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,10 @@ use std::{
     net::TcpStream,
 };
 
-fn main() {
-    let mut stream = TcpStream::connect("127.0.0.1:2020").expect("connection is failed");
-    println!("Connection is established...");
-
+fn command_parsing(mut stream: TcpStream) {
     loop {
+        print!("ftp> ");
+        io::stdout().flush().unwrap();
         let mut input = String::new();
 
         io::stdin()
@@ -16,8 +15,26 @@ fn main() {
 
         stream.write_all(input.as_bytes()).unwrap();
 
-        let mut buffer = String::new();
-        stream.read_to_string(&mut buffer).unwrap();
-        println!("{buffer}");
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+        let response = String::from_utf8_lossy(&buffer);
+        println!("{response}");
+
+        let command: Vec<&str> = input.trim().split_whitespace().collect();
+
+        match command[0] {
+            "quit" => break,
+            _ => continue,
+        }
     }
+}
+
+fn main() {
+    let mut stream = TcpStream::connect("127.0.0.1:2020").expect("connection is failed");
+    println!("Connection is established...");
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let response = String::from_utf8_lossy(&buffer);
+    println!("{response}");
+    command_parsing(stream);
 }
