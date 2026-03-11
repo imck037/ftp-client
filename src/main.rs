7a0c24a0a0e660422223a0de281e33baf3204d57
imck037 <chandradhar.2003@gmail.com>
2026-03-11 12:45:34 +0530

    the client can connect to any given ftp server...

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
 use std::{
+    env,
     io::{self, Read, Write},
     net::TcpStream,
 };
+
+#[derive(Debug)]
+enum ArgError {
+    ArgNotGiven,
+    ArgLenthError,
+}
+
+fn args_parsing(args: env::Args) -> Result<Vec<String>, ArgError> {
+    let args: Vec<String> = args.collect();
+    if args.len() == 1 {
+        return Err(ArgError::ArgNotGiven);
+    } else if args.len() > 3 {
+        return Err(ArgError::ArgLenthError);
+    }
+
+    Ok(args)
+}
 
 fn command_parsing(mut stream: TcpStream) {
     loop {
@@ -29,12 +47,15 @@ fn command_parsing(mut stream: TcpStream) {
     }
 }
 
-fn main() {
-    let mut stream = TcpStream::connect("127.0.0.1:2020").expect("connection is failed");
+fn main() -> Result<(), ArgError> {
+    let args: Vec<String> = args_parsing(env::args())?;
+    let host = format!("{}:{}", args[1], args[2]);
+    let mut stream = TcpStream::connect(host).expect("connection is failed");
     println!("Connection is established...");
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
     let response = String::from_utf8_lossy(&buffer);
     println!("{response}");
     command_parsing(stream);
+    Ok(())
 }
